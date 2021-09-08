02899b5e3500800d0eccc7e5c967dbcade792606
Pavlo Vorobiov <>
2021-09-08 21:28:21 +0300

    Initial commit

---
+++ AsyncLoad/AsyncLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLoadApp.swift
+//  AsyncLoad
+//
+//  Created by Pavlo Vorobiov on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncLoad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncLoad
+//
+//  Created by Pavlo Vorobiov on 9/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("SwiftP")
+                .font(.largeTitle)
+                .padding(.top)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
