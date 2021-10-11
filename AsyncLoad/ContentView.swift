8ee8c94b9ff4b19539ed57d8d8ae2bd7071e0522
Pavlo Vorobiov <>
2021-10-11 08:29:07 +0300

    async function

--- AsyncLoad/ContentView.swift
+++ AsyncLoad/ContentView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var photoService = PService()
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 200))
+    ]
+    
+    
     var body: some View {
         VStack{
-            Text("SwiftP")
-                .font(.largeTitle)
-                .padding(.top)
+            Text("Loading in background...")
+                .font(.subheadline)
+            
+            List{
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(photoService.photoList.list) { photo in
+                        Text("\(photo.author)")
+                        AsyncImage(url: URL(string: photo.download_url)) { image in
+                            image.resizable()
+                                .aspectRatio(contentMode:
+                                                    .fill)
+                        }placeholder: {
+                            Color.blue
+                        }
+                            .frame(width: 200, height: 200)
+                            .clipped()
+                            .cornerRadius(20)
+                    }
+                }.padding()
+            }
+            Button {
+                Task {
+                    try? await photoService.fetchPhotoList()
+                }
+            } label: {
+                Text("More data")
+                    .overlay {
+                        if photoService.isFetching {
+                            ProgressView()
+                        }
+                    }
+            }
+            .padding()
         }
     }
 }
